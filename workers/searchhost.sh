bae38ee76254529b9846b4ab74225510f75aec0c
Thoralf Liersch <thoralf@liersch.info>
2015-09-13 16:30:17 +0200

    translate help text

--- workers/searchhost.sh
+++ workers/searchhost.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-## USAGE: - Durchsucht den Nameserver nach einem Hostnamen und gibt die Aktuelle IP Adresse zurück
-## USAGE: - Wenn der Host nicht gefunden wurde, wird eine entsprechende Meldung ausgegeben
+## USAGE: - searches for a hostname on a nameserver and returns the current ip
 ## ARGUMENT:{:hostname}
 
 ip=$(dig +short $@)
